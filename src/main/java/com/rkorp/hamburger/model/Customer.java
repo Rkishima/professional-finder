7a212132c20834bb3be7450820a2d5bed11a9bb4
RKishima <ricardokishima@hotmail.com>
2022-10-19 00:01:33 -0300

    address CRUD associate to customer

--- src/main/java/com/rkorp/hamburger/model/Customer.java
+++ src/main/java/com/rkorp/hamburger/model/Customer.java
@@ -5,19 +5,24 @@ import lombok.Data;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Map;
 
 @Data
 @Table(name = "customer")
 @Entity
 public class Customer {
     @Id
-    @NotNull
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column
     @NotBlank
     private String name;
+
+    @Column
+    @NotBlank
+    private String cpf;
 
     @Column
     @NotBlank
