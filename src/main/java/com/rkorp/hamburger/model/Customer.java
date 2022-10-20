c9ce6ab520a8d35ada7ac84baaa67a70a029853c
RKishima <ricardokishima@hotmail.com>
2022-10-20 11:10:11 -0300

    fix email and cpf exist validation. change pw

--- src/main/java/com/rkorp/hamburger/model/Customer.java
+++ src/main/java/com/rkorp/hamburger/model/Customer.java
@@ -3,8 +3,10 @@ package com.rkorp.hamburger.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.util.List;
 import java.util.Map;
 
@@ -30,6 +32,8 @@ public class Customer {
 
     @Column
     @NotBlank
+    @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}",
+    flags = Pattern.Flag.CASE_INSENSITIVE)
     private String email;
 
     @Column
--- src/main/java/com/rkorp/hamburger/repository/CustomerRepository.java
+++ src/main/java/com/rkorp/hamburger/repository/CustomerRepository.java
@@ -2,6 +2,13 @@ package com.rkorp.hamburger.repository;
 
 import com.rkorp.hamburger.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface CustomerRepository  extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByCpf(String cpf);
+    Optional<Customer> findByEmail(String email);
 }
